e9d3a8a16c8b0b1be810b1b284038c3a1964550a
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-07-17 08:13:10 +0530

    [Ritesh]add : First Name Validation

---
+++ RegexPattern/PatternRegex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPattern
+{
+    public class PatternRegex
+    {  
+        public void ValidateFirstName(string name)
+        {
+            string firstname = "^[A-Z][a-z]{3,}?";
+            if (Regex.IsMatch(name, firstname))
+            {
+                Console.WriteLine("First Name is Matching With Regex");
+            }
+            else
+            {
+                Console.WriteLine("The Name Is Not Match With Regex");
+            }
+        }
+    }
+}
---
+++ RegexPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A Option");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                case 1:
+                    PatternRegex Pattern = new PatternRegex();
+                    Pattern.ValidateFirstName("Ritesh");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
